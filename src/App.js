1ee097d50fedf86a0fcedb768f165b82e0d7d83a
Vasantha Guru T <161410613+vasanthguru@users.noreply.github.com>
2024-08-13 10:41:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+import RoutingCancel from './component/Routingcancel';
+
+
+function App() {
+  return (
+    <div className="App">
+    <RoutingCancel/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/component/reviewsubmited.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ReviewContext = createContext();
+const Reviewsub=()=>
+{
+    return(
+        <div>
+            <ReviewList/>
+        </div>
+    )
+}
+
+const ReviewList = () => {
+    const reviews = useContext(ReviewContext);
+  
+    return (
+  
+      <ul>
+        {reviews.map((review, index) => (
+          <li key={index}>{review}</li>
+        ))}
+      </ul>
+    );
+  };
+  export default Reviewsub;
